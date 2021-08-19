2a311693f526b3a3cf46b59788cfc9010e5e7f2e
xenodochy23 <79444246+xenodochy23@users.noreply.github.com>
2021-08-19 04:22:22 -0700

    commit

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -14,13 +14,13 @@ const Projects = (props) => {
                 </div>
                 <div className="projects-card">
                     <h2>D2W Logistics</h2>
-                     <a href="#" ><img className="demo" src="/images/d2w-demo.gif" alt="project" width="500" height="350"/></a>
+                        <a href="#" ><img className="demo" src="/images/d2w-demo.gif" alt="project" width="500" height="350"/></a>
                 </div>
                 </div>
                 
             </div>
             <div className="parallax">
-            </div>
+            </div>  
         </div>
     )
 }
