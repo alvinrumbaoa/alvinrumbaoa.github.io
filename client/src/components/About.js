7da89c01cbca3130f786b8dd247f20b293dd531a
xenodochy23 <79444246+xenodochy23@users.noreply.github.com>
2021-08-20 23:55:57 -0700

    update

---
+++ client/src/components/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+const About = () =>{
+
+    return(
+        <div className= "container">
+                <div>
+
+
+                </div>
+        </div>
+    )
+}
+
+
+export default About;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require("cors");
-const port = 8000;   
+const port = 5000;   
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
