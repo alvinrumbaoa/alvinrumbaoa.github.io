20cddfd769c47a08e4c55d6cfe1b8c95a5b3cb5b
xenodochy23 <79444246+xenodochy23@users.noreply.github.com>
2021-08-24 19:29:57 -0700

    commit changes

--- client/src/components/Testimonials.js
+++ client/src/components/Testimonials.js
@@ -11,9 +11,7 @@ const Testimonials = (props) => {
             <div className="parallax">
                 
             </div>
-            <div className="parallax">
-                
-            </div>
+         
         </div>
     )
 }
