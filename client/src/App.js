1de030480a1e854a21cbfb79e1fddd4b022d4c75
xenodochy23 <79444246+xenodochy23@users.noreply.github.com>
2021-08-29 00:32:13 -0700

    media queries and mobile friendly

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import Header from './components/Header';
 import ResumeViewer from './components/ResumeViewer';
 import { Switch, Route,} from "react-router-dom";
+import About from './components/About';
+import Projects from './components/Projects';
 
 function App() {
 
@@ -21,6 +23,8 @@ function App() {
           <Route path ="/alvinrumbaoa.github.io/">
             <Homepage/>
           </Route>
+          <Route path="alvinrumbaoa.github.io/about" component={About} />
+          <Route path="alvinrumbaoa.github.io/projects" component={Projects} />
       </Switch>
 
     </div>
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -13,25 +13,25 @@ const Header = (props) => {
     return(
         <div className= "container">
             <nav className="header-container">
-            <h1 className="logo"><a href="/alvinrumbaoa.github.io/">Alvin Rumbaoa</a></h1>
+            
+            <div> <h1 className="logo"><a href="/alvinrumbaoa.github.io/">Alvin Rumbaoa</a></h1></div>
             <div className="menu-bar">
-            <button onClick={handleToggle}> {navbarOpen ? ( <MdClose style={{ color: "#fff", width: "40px", height: "40px" }} />
+            <button className="hamburger" onClick={handleToggle}> {navbarOpen ? ( <MdClose style={{ color: "#7b7b7b", width: "40px", height: "40px" }} />
             ) : (<FiMenu style={{ color: "#7b7b7b", width: "40px", height: "40px" }} />
             )}     
             </button>           
+            </div>
             <ul className={navbarOpen? "nav-menu" : "hidebar" }>
-                    <li className="list-item"><a href="/alvinrumbaoa.github.io/">Home</a></li>
-                    <li className="list-item"><a href="/alvinrumbaoa.github.io/about">About</a></li>
-                    <li className="list-item"><a href="/alvinrumbaoa.github.io/projects">Works</a></li>
-                    <li className="list-item"><a href="/alvinrumbaoa.github.io/blog">Blog</a></li>
-            </ul>       
-                
-                </div>
-                <ul className="nav-menu2">
-                    <li className="list-item"><a href="/alvinrumbaoa.github.io/">Home</a></li>
+                    <li className="list-item"><Link to="/alvinrumbaoa.github.io/">Home</Link></li>
                     <li className="list-item"><Link to="/alvinrumbaoa.github.io/about">About</Link></li>
-                    <li className="list-item"><a href="/alvinrumbaoa.github.io/projects">Works</a></li>
-                    <li className="list-item"><a href="/alvinrumbaoa.github.io/blog">Blog</a></li>
+                    <li className="list-item"><Link to="/alvinrumbaoa.github.io/projects">Works</Link></li>
+                    <li className="list-item"><Link to="/alvinrumbaoa.github.io/blog">Blog</Link></li>
+            </ul>         
+                <ul className="nav-menu2">
+                <li className="list-item"><Link to="/alvinrumbaoa.github.io/">Home</Link></li>
+                    <li className="list-item"><Link to="/alvinrumbaoa.github.io/about">About</Link></li>
+                    <li className="list-item"><Link to="/alvinrumbaoa.github.io/projects">Works</Link></li>
+                    <li className="list-item"><Link to="/alvinrumbaoa.github.io/blog">Blog</Link></li>
                 </ul>    
             </nav>
         </div>
