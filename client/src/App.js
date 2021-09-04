f32f93c4b3026a18572b7088209f42ae3aa7e1c9
xenodochy23 <79444246+xenodochy23@users.noreply.github.com>
2021-09-03 22:51:51 -0700

    fixed mobile

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import Homepage from './components/Homepage';
 import './App.css';
 import Header from './components/Header';
@@ -6,16 +6,12 @@ import ResumeViewer from './components/ResumeViewer';
 import { Switch, Route,} from "react-router-dom";
 import About from './components/About';
 import Projects from './components/Projects';
-
 function App() {
-  
 
-  useEffect(() => {
-    document.title = "Alvin Rumbaoa | Full Stack Web Developer";
-    }, [])
   return (
   
     <div className="App">
+      
       <Header/>
       <Switch> 
           <Route path="/alvinrumbaoa.github.io/image">
--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,8 @@ import {
   BrowserRouter as Router,
 } from "react-router-dom";
 
+
+
 ReactDOM.render(
   <Router>
     <App />
