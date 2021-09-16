79141c893458d14457e74d05bddc6e80330c2560
Alvin Flores Rumbaoa <79444246+xenodochy23@users.noreply.github.com>
2021-09-15 22:40:14 -0700

    ds

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -43,41 +43,41 @@ const Footer = () =>{
                 <div className="contact-us-container">
                     <h1 className="big-text">Freelance Work or Hire me for Employment</h1>
                                 <h2>Ready and Open for any ideas for your next Project</h2>
-                                <motion.div  whileHover={{ scale: 1.1 }}  whileTap={{ scale: 0.90 }} >
+                                <motion.div  whileHover={{ scale: 1.1 }}  whileTap={{ scale: 0.80 }} >
                                         <a href="https://calendly.com/alvinrumbaoa/30min">
                                             <img src="images/add-video.png" alt="" height="200" width="200"/>
                                             <h2>Schedule with me</h2>
                                         </a>
                                 </motion.div>
                     <div className="social-media">
-                                <motion.div  whileHover={{ scale: 1.1 }}  whileTap={{ scale: 0.90 }} >
+                                <motion.div  whileHover={{ scale: 1.1 }}  whileTap={{ scale: 0.80 }} >
                                         <a href="https://www.facebook.com/xenofy/">
-                                            <img src="images/facebook.png" alt="" height="90" width="90"/>
+                                            <img src="images/facebook.png" alt="" height="80" width="80"/>
                                         </a>
                                 </motion.div>
-                                <motion.div  whileHover={{ scale: 1.1 }}  whileTap={{ scale: 0.90 }} >
+                                <motion.div  whileHover={{ scale: 1.1 }}  whileTap={{ scale: 0.80 }} >
                                 <a href="https://github.com/xenodochy23">
-                                <img src="images/github (2).png" alt="" height="90" width="90"/>
+                                <img src="images/github (2).png" alt="" height="80" width="80"/>
                                 </a>
                                 </motion.div>                            
-                                <motion.div  whileHover={{ scale: 1.1 }}  whileTap={{ scale: 0.90 }} >
+                                <motion.div  whileHover={{ scale: 1.1 }}  whileTap={{ scale: 0.80 }} >
                                         <a href="https://www.instagram.com/iamachipmunks/">
-                                            <img src="images/instagram.png" alt="" height="90" width="90"/>
+                                            <img src="images/instagram.png" alt="" height="80" width="80"/>
                                         </a>
                                 </motion.div>
-                                <motion.div  whileHover={{ scale: 1.1 }}  whileTap={{ scale: 0.90 }} >
+                                <motion.div  whileHover={{ scale: 1.1 }}  whileTap={{ scale: 0.80 }} >
                                     <a href="https://www.twitter.com">
-                                            <img src="images/twitter (1).png" alt="" height="90" width="90"/>
+                                            <img src="images/twitter (1).png" alt="" height="80" width="80"/>
                                     </a>
                                 </motion.div>
-                                <motion.div  whileHover={{ scale: 1.1 }}  whileTap={{ scale: 0.90 }} >
+                                <motion.div  whileHover={{ scale: 1.1 }}  whileTap={{ scale: 0.80 }} >
                                     <a href="https://www.twitter.com">
-                                            <img src="images/linkedin (2).png" alt="" height="90" width="90"/>
+                                            <img src="images/linkedin (2).png" alt="" height="80" width="80"/>
                                     </a>
                                 </motion.div>
-                                <motion.div  whileHover={{ scale: 1.1 }}  whileTap={{ scale: 0.90 }} >
+                                <motion.div  whileHover={{ scale: 1.1 }}  whileTap={{ scale: 0.80 }} >
                                     <a href="mailto:alvinrumbaoa@gmail.com">
-                                            <img src="images/iphone-messages-app.png" alt="" height="90" width="90"/>
+                                            <img src="images/iphone-messages-app.png" alt="" height="80" width="80"/>
                                     </a>
                                 </motion.div>
                     </div>   
