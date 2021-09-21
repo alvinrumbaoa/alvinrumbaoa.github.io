02d656698fc0f90643b7b7dae3dac1789aaed0d5
Maica Rumbaoa <maicarumbaoa@Maicas-MacBook-Pro.local>
2021-09-20 21:14:43 -0700

    resume update

--- client/src/components/ResumeViewer.jsx
+++ client/src/components/ResumeViewer.jsx
@@ -9,7 +9,7 @@ const ResumeViewer = ( ) =>{
  
     return(
         <div className="viewer-container">
-                <Lightbox className="resume-image" image="https://scontent-lax3-2.xx.fbcdn.net/v/t1.6435-9/240738337_4928815487128819_2859854230632875106_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=730e14&_nc_ohc=6_ZM-GMUTNMAX-cenb4&_nc_ht=scontent-lax3-2.xx&oh=0354e5734e9c1d54a4b577704a1fd4f4&oe=615068A8" title="My Resume" onClose ={handleClick}/>
+                <Lightbox className="resume-image" image="https://scontent-lax3-2.xx.fbcdn.net/v/t1.6435-9/242526582_4998109536866080_2128213995673899664_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=730e14&_nc_ohc=7HkmHOBJtxQAX_YVtzD&_nc_ht=scontent-lax3-2.xx&oh=a35cc4daf26c1255e1cdbba1aabe4beb&oe=6170A966" title="My Resume" onClose ={handleClick}/>
         </div>
     )
 }
